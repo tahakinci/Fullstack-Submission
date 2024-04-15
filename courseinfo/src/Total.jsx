c15c3df144f6f528bce0f015310765694c8eb5bd
tahakinci <tahakinci29@gmail.com>
2024-04-15 20:41:00 +0300

    part-1 end of 'JavaScript' section

--- courseinfo/src/Total.jsx
+++ courseinfo/src/Total.jsx
@@ -1,5 +1,6 @@
-const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>;
+const Total = ({ parts }) => {
+  const total = parts.map((part) => part.exercises).reduce((a, b) => a + b);
+  return <p>Number of exercises {total} </p>;
 };
 
 export default Total;

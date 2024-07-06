06e778d36e134c0509c6cb477b25d72bdf3b2e6d
tahakinci <tahakinci29@gmail.com>
2024-07-06 18:50:33 +0300

    setup

--- part-4/phonebook-backend/eslint.config.mjs
+++ part-4/phonebook-backend/eslint.config.mjs
@@ -21,7 +21,7 @@ export default [
     },
 
     rules: {
-      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/indent": ["warning", 2],
       "@stylistic/js/linebreak-style": ["error", "unix"],
       "@stylistic/js/quotes": ["error", "single"],
       "@stylistic/js/semi": ["error", "never"],

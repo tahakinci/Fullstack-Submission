503500de2ae4ca9c6404fd9727842d0c2307d1a4
tahakinci <tahakinci29@gmail.com>
2024-07-31 21:47:25 +0300

    Exercises 4.8 - 4.12 complated

--- part-4/bloglist/controllers/blogs.js
+++ part-4/bloglist/controllers/blogs.js
@@ -1,27 +1,32 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs);
-  });
+blogRouter.get("/", async (req, res) => {
+  const notes = await Blog.find({});
+
+  res.json(notes);
 });
 
-blogRouter.post("/", (req, res, next) => {
-  const body = req.body;
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  });
+blogRouter.post("/", async (req, res, next) => {
+  try {
+    const body = req.body;
 
-  blog
-    .save()
-    .then((result) => {
-      res.json(blog);
-    })
-    .catch((e) => next(e));
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes ? body.likes : 0,
+    });
+
+    const savedBlog = await blog.save();
+    console.log("here: ", savedBlog);
+    res.status(201).json(savedBlog);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ error: error.message });
+    }
+    next(error);
+  }
 });
 
 module.exports = blogRouter;
--- part-4/bloglist/models/blog.js
+++ part-4/bloglist/models/blog.js
@@ -13,9 +13,18 @@ mongoose
   });
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
   likes: Number,
 });
 blogSchema.set("toJSON", {
